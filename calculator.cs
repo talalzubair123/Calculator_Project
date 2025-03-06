bbd15649a430987b082aef5072a297ad5aea8e88
talalzubair123 <talalmirza317@gmail.com>
2025-03-06 09:06:59 +0500

    Create calculator.cs

---
+++ calculator.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Name: Your Name");
+        Console.WriteLine("Roll Number: Your Roll Number");
+    }
+}
